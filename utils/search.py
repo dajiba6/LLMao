e4f25dc82ce0d5d7a642f65da8f0de00c28b7e86
cyn <chao.yn@foxmail.com>
2025-03-12 09:27:35 +0800

    change number of result

--- utils/search.py
+++ utils/search.py
@@ -3,9 +3,9 @@ from duckduckgo_search import DDGS
 
 def duckducknews(query: str) -> list:
     with DDGS() as ddgs:
-        return list(ddgs.news(keywords=query, region="cn-zh", max_results=3))
+        return list(ddgs.news(keywords=query, region="cn-zh", max_results=5))
 
 
 def duckducktext(query: str) -> list:
     with DDGS() as ddgs:
-        return list(ddgs.text(keywords=query, region="cn-zh", max_results=3))
+        return list(ddgs.text(keywords=query, region="cn-zh", max_results=5))
